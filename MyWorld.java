4adf7f541ae5c7f3ac417f2b2b887a5efef411f2
yyyannnn <349168872@gapps.yrdsb.ca>
2023-12-01 08:48:53 -0500

    Set speed for fish

--- MyWorld.java
+++ MyWorld.java
@@ -4,7 +4,7 @@ public class MyWorld extends World
 {
     public int score = 0;
     Label scoreLabel;
-    
+    int level = 2;
     
     public MyWorld()
     {    
@@ -40,11 +40,17 @@ public class MyWorld extends World
     {
         score++;
         scoreLabel.setValue(score);
+        
+        if (score % 5 == 0)
+        {
+            level++;
+        }
     }
     
     // Create fish anywhere at the top of the screen
     public void createFish(){
         Fish fish = new Fish();
+        fish.setSpeed(level);
         int x = Greenfoot.getRandomNumber(600);
         int y = 0;
         addObject(fish,x,y);
